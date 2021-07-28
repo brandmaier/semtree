b723e4a5eb60ed40ca5c89bfb594d1f1633058b7
Andreas Brandmaier <brandmaier@mpib-berlin.mpg.de>
2021-07-28 14:04:38 +0200

    added predict.semtree() generic

---
+++ R/predict.semtree.R
@@ -0,0 +1,26 @@
+
+#' 
+#' @param object semtree. A SEM tree node.
+#' @param data a data.frame with observations used for prediction
+#' @param type String. What type of prediction should be made? Currently, this
+#' only supports 'node_id', which returns the id of a terminal node.
+#' @exportS3Method predict semtree
+#' @author Caspar van Lissa, Andreas Brandmaier
+#' 
+predict.semtree <- function(object, data, type = "node_id", ...) {
+  
+  if(!hasArg(data)){
+    if(inherits(object$model, "MxModel")){
+      data <- object$model$data$observed
+    } else {
+      ui_stop("Argument 'data' required.")
+    }
+  }
+  result <- switch(type,
+                   "node_id" = { traverse(object, dataset = data)}
+  )
+  
+  if (is.null(result)) ui_stop("Requested type no yet implemented in predict.semtree().")
+  
+  return(result)
+} 
--- R/predict_tree.R
+++ R/predict_tree.R
@@ -3,7 +3,6 @@ evaluateTreePrediction <- function(tree, test_set, leaf_ids=NULL)
   
   if (is.null(tree)) return(NULL)
   if (is.null(test_set)) return(NULL)
-  #model_ids <- tree$model.ids;
   
   mapping <- getIdParameterMapping(tree)
   
@@ -15,20 +14,14 @@ evaluateTreePrediction <- function(tree, test_set, leaf_ids=NULL)
   # for each leaf, calculate deviance of each data row
   error <- 0
   cnt <- 0
-  #leafwise <- rep(NA, length(unique(leaf_ids)))
-  #dev <- 0
+
   for (leaf_id in unique(leaf_ids))
   {
-    #print(leaf_id)
     temp_set <- test_set[leaf_ids==leaf_id, ];
-    
-    #params <- mapping[ mapping[,1]==leaf_id, 2:ncol(mapping)]
-    #model <- set_model_parameters(tree$model, params)
     
     leaf <- getNodeById( tree, leaf_id)
     model <- leaf$model
-    #add_dev <- evaluateDataLikelihood(leaf$model, temp_set[,,drop=F], data_type )
-    
+
     # get model mean
     templen <- dim(temp_set)[1]
    
@@ -40,10 +33,8 @@ evaluateTreePrediction <- function(tree, test_set, leaf_ids=NULL)
       localerror <- sum(row.value-ram.mean, na.rm=T)^2
       error <- error + localerror
       cnt <- cnt + length(row.value)-is.na(row.value)
-     # cat(row.value," ",ram.mean," Err: ",localerror,"  ",temp_set$x[j],"row#",rownames(temp_set)[j],"\n")
     }
-    #cat(paste("Add deviance ",add_dev,"leaf",leaf$node_id,"\n")); ##, toString(which(leaf_ids==leaf_id)) ,"\n"))
-#    dev <- dev + add_dev;
+
   }
   
   result <- list()
