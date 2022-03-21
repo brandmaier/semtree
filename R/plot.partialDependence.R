c38d5026434522c64e954e51dbee05590c16bb73
Andreas Brandmaier <andy@brandmaier.de>
2022-03-21 20:42:58 +0100

    improved interaction plots

--- R/plot.partialDependence.R
+++ R/plot.partialDependence.R
@@ -10,27 +10,74 @@ plot_partialDependence <-
       stop("Please specify argument 'parameter'!")
     }
     
-    if (is.null(xlab)) {
-      xlab <- x$reference.var
-    }
+    num_reference_vars = length(x$reference.var)
     
-    if (is.null(ylab)) {
-      ylab <- parameter
-    }
-    
-    is_fac = is.factor(x$samples[[x$reference.var]])
-    
-    if (!is_fac) {
-      ggplot2::ggplot(x$samples,
-                      ggplot2::aes_string(x = x$reference.var, y = parameter)) +
-        ggplot2::geom_line() + ggplot2::theme_light() + ggplot2::ggtitle("Partial Dependence Plot")
+    if (num_reference_vars == 1) {
+      if (is.null(xlab)) {
+        xlab <- x$reference.var
+      }
+      
+      if (is.null(ylab)) {
+        ylab <- parameter
+      }
+      
+      is_fac = is.factor(x$samples[[x$reference.var]])
+      
+      if (!is_fac) {
+        ggplot2::ggplot(x$samples,
+                        ggplot2::aes_string(x = x$reference.var, y = parameter)) +
+          ggplot2::geom_line() + ggplot2::theme_light() + ggplot2::ggtitle("Partial Dependence Plot")
+      } else {
+        ggplot2::ggplot(
+          x$samples,
+          ggplot2::aes_string(
+            x = x$reference.var,
+            y = parameter,
+            fill = x$reference.var
+          )
+        ) +
+          ggplot2::geom_bar(stat = "identity") +
+          ggplot2::theme_light() + ggplot2::ggtitle("Partial Dependence Plot")
+      }
+      
+    } else if (num_reference_vars == 2) {
+      is_fac1 = is.factor(x$samples[[x$reference.var[1]]])
+      is_fac2 = is.factor(x$samples[[x$reference.var[2]]])
+      
+      if (is_fac1 && !is_fac2) { # swap factor to second position
+        is_fac1 = is_fac2
+        is_fac2 = TRUE
+        temp = x$reference.var[1]
+        x$reference.var[1] = x$reference.var[2]
+        x$reference.var[2] = temp
+      }
+      
+      if (!is_fac1) {
+        gp <- ggplot2::ggplot(x$samples,
+                              ggplot2::aes_string(x = x$reference.var[1], y = parameter)) +
+          ggplot2::geom_line() + ggplot2::theme_light() + 
+          ggplot2::ggtitle("Partial Dependence Plot")
+      } else {
+        gp <- ggplot2::ggplot(
+          x$samples,
+          ggplot2::aes_string(
+            x = x$reference.var[1],
+            y = parameter,
+            fill = x$reference.var[1]
+          )
+        ) +
+          ggplot2::geom_bar(stat = "identity") +
+          ggplot2::theme_light() + ggplot2::ggtitle("Partial Dependence Plot")
+      }
+      
+      if (is_fac2) {
+        gp <- gp + facet_wrap(x$reference.var[2])
+      } else {
+        ui_stop("Plots are currently only supported if at least one reference variable is a factor.")
+      }
+      
+      return(gp)
     } else {
-      ggplot2::ggplot(
-        x$samples,
-        ggplot2::aes_string(x = x$reference.var, y = parameter,
-                            fill = x$reference.var)
-      ) +
-        ggplot2::geom_bar(stat = "identity") +
-        ggplot2::theme_light() + ggplot2::ggtitle("Partial Dependence Plot")
+      ui_stop("Plots are supported with only up to 2 reference variables.")
     }
   }
