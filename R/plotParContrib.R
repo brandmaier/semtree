43a3f64608e57f413a6f7e65a2440a6a88682002
Manuel <arnoldmz@hu-berlin.de>
2022-06-28 17:53:03 +0200

    Added getParContrib and plotParContrib functions to extract and visualize parameter contributions to score-based test statistics.

---
+++ R/plotParContrib.R
@@ -0,0 +1,35 @@
+#' @title Plot a heatmap of the parameter contributions to the score-based test
+#' statistics
+#' @description Plots a heatmap with the standardized contributions of the model
+#'  parameters to the score-based test statistics.
+#' @param tree a semtree object.
+#' @author Manuel Arnold
+#' @export
+
+plotParContrib <- function(tree) {
+  
+  stopifnot("Score-guided SEM tree required" = tree$control$method == "score")
+  
+  par_contrib <- getParContrib(tree = tree, standardized = TRUE)
+  
+  par_contrib <- t(par_contrib)
+  
+  df <- as.data.frame(matrix(NA, nrow = nrow(par_contrib) * ncol(par_contrib),
+                             ncol = 3))
+  colnames(df) <- c("Node", "Parameter", "Contribution")
+  
+  for (i in 1:nrow(par_contrib)) {
+    for (j in 1:ncol(par_contrib)) {
+      df[((i-1)*ncol(par_contrib)+1):(i*ncol(par_contrib)), 1] <- rownames(par_contrib)[i]
+      df[(i-1)*ncol(par_contrib)+j, 2] <- colnames(par_contrib)[j]
+      df[(i-1)*ncol(par_contrib)+j, 3] <- par_contrib[i, j]
+    }
+  }
+  
+  ggplot2::ggplot(df, ggplot2::aes(x = Node, Parameter)) +
+    ggplot2::geom_tile(ggplot2::aes(fill = Contribution)) +
+    ggplot2::scale_fill_gradient(low = "white", high = "red") +
+    ggplot2::geom_text(ggplot2::aes(label = round(Contribution, 3))) +
+    ggplot2::theme_bw()
+  
+}
