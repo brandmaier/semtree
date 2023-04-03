985c95fb24c527f6d7f693fc158031278985824e
Andreas Brandmaier <andy@brandmaier.de>
2023-04-03 13:46:51 +0200

    heuristic to extract definition variables

---
+++ R/findDefinitionVariables.R
@@ -0,0 +1,15 @@
+findDefinitionVariables <- function(model) {
+  def_vars <- c()
+  for (mid in 1:length(m@matrices)) {
+    mt <- model@matrices[[mid]]
+    idx <- startsWith(mt$labels,"data.")
+    if (any(idx,na.rm = TRUE)) {
+      idx[is.na(idx)]<-FALSE
+      labs <- mt$labels[idx]
+      labs <- sapply(labs, function(x){ substr(x,6,nchar(x))})
+      def_vars <- c(def_vars, labs)
+    }
+  }
+  
+  return(def_vars)
+}
