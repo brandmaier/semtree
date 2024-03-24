e6e5a63b29797b6212e709fd0dad18587e75e30a
Andreas M. Brandmaier <andy@brandmaier.de>
2024-03-24 21:40:28 +0100

    added styling using styler package

--- R/getLikelihood.R
+++ R/getLikelihood.R
@@ -1,15 +1,13 @@
-getLikelihood<-function(model)
-{
+getLikelihood <- function(model) {
   # this seems to be not generic enough
   #  return(OpenMx::mxEval(objective, model));
-  
-  msm <- getS3method("summary","MxModel")
+
+  msm <- getS3method("summary", "MxModel")
   # alternative:
   if (is.null(model)) {
     warning("NULL Model in getLikelihood()-call")
     return(NULL)
   }
-  
+
   return(msm(model)$Minus2LogLikelihood)
-  
-  }
+}
--- R/print.semtree.R
+++ R/print.semtree.R
@@ -3,38 +3,33 @@ print.semtree <-
   function(x,
            level = 0,
            p.values.valid = NULL,
-           ...)
-  {
+           ...) {
     tree <- x
-    
+
     indent <- paste(rep("|   ", level), collapse = "", sep = "")
-    
+
     if (level > 0) {
       edge_label <- tree$edge_label
     } else {
       edge_label <- "ROOT"
-      
     }
-    
+
     caption <- tree$caption
-    
-    if (caption == "TERMINAL")
-    {
-      #caption <- paste(caption,"(ID=",tree$node_id,")",sep="")
+
+    if (caption == "TERMINAL") {
+      # caption <- paste(caption,"(ID=",tree$node_id,")",sep="")
       output <-
         paste(indent,
-              "|-[",
-              tree$node_id,
-              "] ",
-              caption,
-              " [N=",
-              tree$N,
-              "]\n",
-              sep = "")
-      
-    }
-    
-    else {
+          "|-[",
+          tree$node_id,
+          "] ",
+          caption,
+          " [N=",
+          tree$N,
+          "]\n",
+          sep = ""
+        )
+    } else {
       output <-
         paste(
           indent,
@@ -52,28 +47,23 @@ print.semtree <-
           "]\n",
           sep = ""
         )
-      
     }
-    
-    if (tree$caption != "TERMINAL")
-    {
+
+    if (tree$caption != "TERMINAL") {
       output <-
         paste(
           output,
           print.semtree(tree$left_child, level + 1, p.values.valid),
           print.semtree(tree$right_child, level + 1, p.values.valid)
         )
-      
     }
-    
+
     if (level == 0) {
       output <- paste("SEMtree with numbered nodes\n", output)
-      
+
       cat(output)
-      
     } else {
       return(output)
-      
     }
   }
 
