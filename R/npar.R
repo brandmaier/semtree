b650ecb3b33130d081b1b7205435e20105e06310
Andreas M. Brandmaier <andy@brandmaier.de>
2024-03-24 13:45:25 +0100

    ran through styler

--- R/npar.R
+++ R/npar.R
@@ -1,11 +1,11 @@
 npar <- function(x) {
-  if (is(x,"OpenMx") || is(x,"MxRAMModel")) {
-    stop("npar() not implemented yet")
+  if (is(x, "OpenMx") || is(x, "MxRAMModel")) {
+    summary(x)$estimatedParameters
   } else if (is(x, "ctsemFit")) {
-    stop("npar() not implemented yet")
-  } else if (is(x,"lavaan")) {
-    pt <- parameterTable(x)
-    sum(pt$free!=0)
+    stop("npar() not implemented yet for ctsemFit")
+  } else if (is(x, "lavaan")) {
+    pt <- lavaan::parameterTable(x)
+    sum(pt$free != 0)
   } else {
     stop("npar() not implemented yet")
   }
