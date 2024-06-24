56695bb311ac3712b96509d60c3ad685dfbc3d2a
trbrick <tbrick@psu.edu>
2024-06-24 21:58:19 +0200

    Minor updates to simplify the boruta test.

--- tests/borutaOneIndicator.R
+++ tests/borutaOneIndicator.R
@@ -3,18 +3,23 @@
 library(OpenMx)
 
 N <- 1000
-influence <- c(1, 0, 0, 0)
+influence <- c(1, 0)
 nPred <- length(influence)
+cutbreaks <- 3
 
 
 genModel <- mxModel(type="RAM", manifestVars=c("Y", paste0("X", 1:nPred)),
                     mxPath(paste0("X", 1:nPred), "Y", values=influence),
                     mxPath(c("Y", paste0("X", 1:nPred)), arrows=2, values=1))
 simpleData <- mxGenerateData(genModel, 1000)
+for(i in paste0("X", 1:nPred)) {
+  simpleData[[i]] <- cut(simpleData[[i]], cutbreaks)
+}
 
 testModel <- mxModel("SimplisticModel", type="RAM", manifestVars="Y", 
                      mxPath("Y", arrows=2, values=1, free=TRUE),
                      mxPath("one", "Y", values=0, free=TRUE), 
                      mxData(simpleData, type="raw"))
 
-boruta(testModel, simpleData, verbose=TRUE)
+output <- boruta(testModel, simpleData, verbose=TRUE)
+output
