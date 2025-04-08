9a7926a3bf1e5f51959f1be17a1319e7167d49c5
Andreas M. Brandmaier <andy@brandmaier.de>
2025-04-08 13:41:43 +0200

    some reformatting of code

--- R/findOtherSplits.R
+++ R/findOtherSplits.R
@@ -1,11 +1,11 @@
 #' Find Other Node Split Values
-#' 
+#'
 #' Search tool to search nodes for alternative splitting values found during
 #' the \code{\link{semtree}} process. Given a particular node, competing split
 #' values are listed assuming they also meet the criteria for a significant
 #' splitting value as set by \code{\link{semtree.control}}.
-#' 
-#' 
+#'
+#'
 #' @param node A node from a \code{\link{semtree}} object.
 #' @param tree A \code{\link{semtree}} object which the node is part of.
 #' @return A \code{data.frame()} with rows corresponding to the variable names
@@ -16,28 +16,27 @@
 #' U. (2013). Structural equation model trees. \emph{Psychological Methods},
 #' 18(1), 71-86.
 findOtherSplits <-
-function(node, tree)
-{
-	if (tree$p.values.valid) {
-		idx <- which(node$p_values <= tree$options$alpha)
-	} else {
-		idx <- which(node$lr_values > 0)
-	}
-	
-	idx2 <- node$covariate.ids[idx]
-	
-	cvalues <- as.numeric( round( node$lr_values[idx], 3) )
-	cnames <- names(tree$recoding$dataset)[idx2]
-	
-	#cat(cvalues)
-	#cat(cnames)
-	#print(cvalues)
-	
-	df<-data.frame( cbind(cnames,  cvalues ) )
-	df<-data.frame( cnames, cvalues  )
-	
-	#df <- sort(df, by =~ df[,2])
-	df <- df[with(df,order(-df[,2])) ,]
-	
-	return(df)
-}
+  function(node, tree) {
+    if (tree$p.values.valid) {
+      idx <- which(node$p_values <= tree$options$alpha)
+    } else {
+      idx <- which(node$lr_values > 0)
+    }
+
+    idx2 <- node$covariate.ids[idx]
+
+    cvalues <- as.numeric(round(node$lr_values[idx], 3))
+    cnames <- names(tree$recoding$dataset)[idx2]
+
+    # cat(cvalues)
+    # cat(cnames)
+    # print(cvalues)
+
+    df <- data.frame(cbind(cnames, cvalues))
+    df <- data.frame(cnames, cvalues)
+
+    # df <- sort(df, by =~ df[,2])
+    df <- df[with(df, order(-df[, 2])), ]
+
+    return(df)
+  }
--- R/getHeight.R
+++ R/getHeight.R
@@ -1,43 +1,41 @@
 #' Determine Height of a Tree
-#' 
+#'
 #' Returns height of a SEM Tree, which equals to the length of the longest path
 #' from root to a terminal node.
-#' 
-#' 
+#'
+#'
 #' @param tree A SEM tree.
 #' @author Andreas M. Brandmaier, John J. Prindle
 #' @references Brandmaier, A.M., Oertzen, T. v., McArdle, J.J., & Lindenberger,
 #' U. (2013). Structural equation model trees. \emph{Psychological Methods},
 #' 18(1), 71-86.
 
-getHeight <- function(tree)
-{
-  if ((is.null(tree$left_child)) && (is.null(tree$right_child))) 	
-  {
-    return(1);
+getHeight <- function(tree) {
+  if ((is.null(tree$left_child)) && (is.null(tree$right_child))) {
+    return(1)
   }
-  
+
   if (tree$left_child$caption != "TERMINAL") {
-    countl <- 1+ getHeight(tree$left_child)
+    countl <- 1 + getHeight(tree$left_child)
   } else {
-    countl <- 2	
+    countl <- 2
   }
   if (tree$right_child$caption != "TERMINAL") {
-    countr <- 1+ getHeight(tree$right_child)
+    countr <- 1 + getHeight(tree$right_child)
   } else {
-    countr <- 2	
-  }	
-  
-  return( max(countl, countr) )
+    countr <- 2
+  }
+
+  return(max(countl, countr))
 }
 
 
 
 #' Get the depth (or, height) a tree.
-#' 
+#'
 #' Returns the length of the longest path from a root node to a leaf node.
-#' 
-#' 
+#'
+#'
 #' @param tree A \code{\link{semtree}} object
 #' @author Andreas M. Brandmaier
 #' @references Brandmaier, A.M., Oertzen, T. v., McArdle, J.J., & Lindenberger,
