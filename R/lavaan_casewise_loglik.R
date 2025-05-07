06ea874e9c2db9768b1f735e882953af3fa808a6
brandmaier <brandmaier@mpib-berlin.mpg.de>
2025-05-07 16:09:17 +0200

    added support for missing data

--- R/lavaan_casewise_loglik.R
+++ R/lavaan_casewise_loglik.R
@@ -1,9 +1,28 @@
-
+lavaan_casewise_loglik_matrices <- function(Y, mu = NULL, Sigma = NULL) {
+  if (!all(complete.cases(Y))) {
+    # this is slow but should work
+    # TODO: ideally, we would sort the patterns of missing data in groups
+    # first and then prepare mu and Sigma only once
+    apply(Y, 1, function(row) {
+      
+      pattern <- !is.na(row)
+      row <- row[pattern]
+      mu = mu[pattern]
+      Sigma = Sigma[pattern,pattern]
+      .lavaan_casewise_loglik_matrices(row, mu, Sigma)
+      
+    })
+  } else {
+    .lavaan_casewise_loglik_matrices(Y, mu, Sigma)
+  }
+}  
 # This function was contributed by Yves Rosseel and later modified
 # assumes a multivariate normal model without any fancy modifications
 # assumes complete data
-lavaan_casewise_loglik_matrices <- function(Y, mu = NULL, Sigma = NULL) {
+.lavaan_casewise_loglik_matrices <- function(Y, mu = NULL, Sigma = NULL) {
 
+
+  
   Y <- as.matrix(Y)
   if (ncol(Y) == 1L && nrow(Y) == nrow(Sigma)) {
     # just a vector
